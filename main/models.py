86f87f011487c848fbc7cc6499e4cb9f353754de
WTrygar <chandelier.wt@gmail.com>
2022-03-15 10:22:29 +0100

    user specific view

--- main/models.py
+++ main/models.py
@@ -1,10 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
 class ToDoList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todolist", null=True)
     name = models.CharField(max_length=200)
 
     def __str__(self):
